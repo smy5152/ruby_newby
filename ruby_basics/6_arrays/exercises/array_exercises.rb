8641a5f37f6faa188b3492c207e328d7c7bdb18d
smy5152 <shabbiryamani@gmail.com>
2021-02-09 00:58:17 +0200

    Arrays: Exercises & Test rspec complete

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,65 +1,90 @@
 def nil_array(number)
   # return an array containing the number of nil values
+  return Array.new(number)
 end
 
 def nested_array(number)
   # return an array containing the number of empty arrays
+  a=Array.new()
+  for i in 0...number
+    a.append(Array.new())
+  end 
+  return a
 end
 
 def first_element(array)
   # return the first element of the array
+  return array.first
 end
 
 def third_element(array)
   # return the third element of the array
+  if array.length>2
+    return array[2]
+  else
+    return nil
+  end
 end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
+  return array.last(3)
 end
 
 def add_element(array)
   # add an element (of any value) to the array
+  return array.push("new")
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
+  array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  array
+  return array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+  array.shift(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  return original + additional
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  return original.difference(comparison)
 end
 
 def empty_array?(array)
   # return true if the array is empty
+  return array.empty? ? true : false
 end
 
 def reverse(array)
   # return the reverse of the array
+  return array.reverse
 end
 
 def array_length(array)
   # return the length of the array
+  return array.length
 end
 
 def include?(array, value)
   # return true if the array includes the value
+  if array.include?(value)
+    return true
+  else
+    return false
+  end
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
+  array.join(separator)
 end
